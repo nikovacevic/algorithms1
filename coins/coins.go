898a03348b93c03589d6b34ef788e1346da12a39
Niko Kovacevic <nikovacevic@gmail.com>
2016-10-26 21:39:05 -0600

    Implemented a much stronger algorithm for Combinations.

--- coins/coins.go
+++ coins/coins.go
@@ -1,6 +1,6 @@
 package coins
 
-import "errors"
+import "sort"
 
 const maxUint = ^uint(0)
 const maxInt = int(maxUint >> 1)
@@ -12,7 +12,39 @@ type Coin struct {
 }
 
 // Coins is a slice of Coins
-type Coins map[int]Coin
+type Coins []Coin
+
+// ByValue implements sort.Interface for Coins based on Coin.value
+type ByValue Coins
+
+func (a ByValue) Len() int           { return len(a) }
+func (a ByValue) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByValue) Less(i, j int) bool { return a[i].value < a[j].value }
+
+// ByQuantity implements sort.Interface for Coins based on Coin.Quantity
+type ByQuantity Coins
+
+func (a ByQuantity) Len() int           { return len(a) }
+func (a ByQuantity) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByQuantity) Less(i, j int) bool { return a[i].Quantity < a[j].Quantity }
+
+// Equals tests equality of two Coin instances
+func (a Coin) Equals(b Coin) bool {
+	return a.value == b.value && a.Quantity == b.Quantity
+}
+
+// Equals tests equality of two sets of Coins. Order matters!
+func (a Coins) Equals(b Coins) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if !(a[i].value == b[i].value && a[i].Quantity == b[i].Quantity) {
+			return false
+		}
+	}
+	return true
+}
 
 // Minimum takes an amount and a set of Coins (quantities ignored) and returns
 // the Coins with minimum total quantity that sum to amount.
@@ -43,8 +75,7 @@ func Minimum(amount int, coins Coins) (Coins, int) {
 	count := 0
 	for amount > 0 {
 		i := s[amount]
-		inc := coins[i]
-		inc.Quantity++
+		coins[i].Quantity++
 		count++
 		amount -= coins[i].value
 	}
@@ -52,50 +83,25 @@ func Minimum(amount int, coins Coins) (Coins, int) {
 	return coins, count
 }
 
-// Combinations takes an amount and a set of Coins (quantities ignored) and
-// returns the number of unique combinations of those coins that sum to amount.
-func Combinations(amount int, coins Coins) (int, error) {
-	for _, c := range coins {
-		c.Quantity = 0
-	}
-
-	C := make(map[int][]Coins)
-	C[0] = append(C[0], coins.duplicate())
-
-	// n
-	for n := 0; n <= amount; n++ {
-		// n^c
-		for _, combo := range C[n] {
-		CombinationLoop:
-			// c
-			for v, c := range coins {
-				for _, cc := range combo {
-					if cc.value > c.value && cc.Quantity > 0 {
-						continue CombinationLoop
-					}
-				}
-				if n+c.value <= amount {
-					nextCombo := combo.duplicate()
-					inc := nextCombo[v]
-					inc.Quantity++
-					nextCombo[v] = inc
-					C[n+c.value] = append(C[n+c.value], nextCombo)
-				}
-			}
+// Combinations takes an amount and a slice of Coins and returns the number of
+// unique combinations of those coins that sum the given amount.
+func Combinations(amount int, coins Coins) int {
+	// Sort coins by value
+	sort.Sort(ByValue(coins))
+	// Map of amount to combination count
+	C := make(map[int]int)
+	C[0] = 1
+	// Iterate over all coins, rom least to greatest value, checking
+	// combination counts for all possible amounts (C[amt]) for the given
+	// coin (i.e. those greater than or equal to the coin's value) and less
+	// than the given amount. For any C[amt] and coin, C[amt-coin.value]
+	// represents the count of coin combinations necessary to get to the
+	// amount one coin.value less than amount (i.e. the previous step).
+	for _, coin := range coins {
+		for amt := coin.value; amt <= amount; amt++ {
+			C[amt] += C[amt-coin.value]
 		}
 	}
-
-	combos, ok := C[amount]
-	if !ok {
-		return 0, errors.New("No defined solution")
-	}
-	return len(combos), nil
-}
-
-func (coins Coins) duplicate() Coins {
-	c := Coins{}
-	for val, coin := range coins {
-		c[val] = Coin{coin.value, coin.Quantity}
-	}
-	return c
+	// Return the combination count at the given amount
+	return C[amount]
 }
--- coins/coins_test.go
+++ coins/coins_test.go
@@ -1,16 +1,19 @@
 package coins
 
-import "testing"
+import (
+	"sort"
+	"testing"
+)
 
 var MinimumTests = []struct {
 	amount int
 	coins  Coins
 	exp    int
 }{
-	{7, Coins{1: Coin{1, 0}, 2: Coin{2, 0}, 3: Coin{3, 0}}, 3},
-	{9, Coins{2: Coin{2, 0}, 3: Coin{3, 0}}, 3},
-	{10, Coins{1: Coin{1, 0}, 2: Coin{2, 0}, 3: Coin{3, 0}}, 4},
-	{100, Coins{2: Coin{2, 0}, 3: Coin{3, 0}, 10: Coin{10, 0}, 46: Coin{46, 0}}, 5},
+	{7, Coins{Coin{1, 0}, Coin{2, 0}, Coin{3, 0}}, 3},
+	{9, Coins{Coin{2, 0}, Coin{3, 0}}, 3},
+	{10, Coins{Coin{1, 0}, Coin{2, 0}, Coin{3, 0}}, 4},
+	{100, Coins{Coin{2, 0}, Coin{3, 0}, Coin{10, 0}, Coin{46, 0}}, 5},
 }
 
 func TestMinimum(t *testing.T) {
@@ -22,30 +25,43 @@ func TestMinimum(t *testing.T) {
 	}
 }
 
+var SortByValueTests = []struct {
+	coins Coins
+	exp   Coins
+}{
+	{Coins{Coin{1, 0}, Coin{2, 0}, Coin{3, 0}}, Coins{Coin{1, 0}, Coin{2, 0}, Coin{3, 0}}},
+	{Coins{Coin{3, 0}, Coin{2, 0}, Coin{1, 0}}, Coins{Coin{1, 0}, Coin{2, 0}, Coin{3, 0}}},
+	{Coins{Coin{3, 0}, Coin{1, 0}, Coin{3, 0}}, Coins{Coin{1, 0}, Coin{3, 0}, Coin{3, 0}}},
+}
+
+func TestSortByValue(t *testing.T) {
+	for _, tt := range SortByValueTests {
+		sort.Sort(ByValue(tt.coins))
+		if !tt.coins.Equals(tt.exp) {
+			t.Errorf("sort.Sort(ByValue(%v)) expected %v, actual %v", tt.coins, tt.exp, tt.coins)
+		}
+	}
+}
+
 var CombinationsTests = []struct {
 	amount int
 	coins  Coins
 	exp    int
 }{
-	{amount: 1, coins: Coins{1: Coin{1, 0}, 2: Coin{2, 0}, 3: Coin{3, 0}}, exp: 1},
-	{amount: 7, coins: Coins{1: Coin{1, 0}, 2: Coin{2, 0}, 3: Coin{3, 0}}, exp: 8},
-	{amount: 20, coins: Coins{1: Coin{1, 0}, 2: Coin{2, 0}, 3: Coin{3, 0}}, exp: 44},
-	{amount: 30, coins: Coins{1: Coin{1, 0}, 2: Coin{2, 0}, 3: Coin{3, 0}}, exp: 91},
-	{amount: 40, coins: Coins{1: Coin{1, 0}, 2: Coin{2, 0}, 3: Coin{3, 0}}, exp: 154},
-	{amount: 80, coins: Coins{1: Coin{1, 0}, 2: Coin{2, 0}, 3: Coin{3, 0}}, exp: 574},
-	{amount: 80, coins: Coins{1: Coin{1, 0}, 2: Coin{2, 0}, 3: Coin{3, 0}, 4: Coin{4, 0}, 5: Coin{5, 0}, 6: Coin{6, 0}}, exp: 69624},
-	//{amount: 80, coins: Coins{1: Coin{1, 0}, 2: Coin{2, 0}, 3: Coin{3, 0}, 4: Coin{4, 0}, 5: Coin{5, 0}, 6: Coin{6, 0}, 7: Coin{7, 0}, 8: Coin{8, 0}}, exp: 69624},
-	//{amount: 300, coins: Coins{1: Coin{1, 0}, 5: Coin{5, 0}, 10: Coin{10, 0}, 20: Coin{20, 0}}, exp: 5456},
+	{0, Coins{Coin{1, 0}, Coin{2, 0}, Coin{3, 0}}, 1},
+	{1, Coins{Coin{1, 0}, Coin{2, 0}, Coin{3, 0}}, 1},
+	{7, Coins{Coin{1, 0}, Coin{2, 0}, Coin{3, 0}}, 8},
+	{80, Coins{Coin{1, 0}, Coin{2, 0}, Coin{3, 0}, Coin{4, 0}, Coin{5, 0}, Coin{6, 0}}, 69624},
+	{300, Coins{Coin{1, 0}, Coin{5, 0}, Coin{10, 0}, Coin{20, 0}}, 5456},
+	{3000, Coins{Coin{1, 0}, Coin{5, 0}, Coin{10, 0}, Coin{20, 0}}, 4590551},
+	{3000, Coins{Coin{10, 0}, Coin{1, 0}, Coin{5, 0}, Coin{20, 0}}, 4590551},
 }
 
 func TestCombinations(t *testing.T) {
 	for _, tt := range CombinationsTests {
-		if act, err := Combinations(tt.amount, tt.coins); err != nil {
+		act := Combinations(tt.amount, tt.coins)
+		if act != tt.exp {
 			t.Errorf("Combinations(%v, %v) expected %v, actual %v", tt.amount, tt.coins, tt.exp, act)
-		} else {
-			if act != tt.exp {
-				t.Errorf("Combinations(%v, %v) expected %v, actual %v", tt.amount, tt.coins, tt.exp, act)
-			}
 		}
 	}
 }
